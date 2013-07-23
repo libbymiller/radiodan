9476c069721bacd7038b11f512127bff96d5da89
Libby Miller <libby.miller@bbc.co.uk>
2013-07-23 18:02:19 +0100

    make default volume lower; add in a change channel class for middleware

---
+++ lib/radiodan/middleware/change_channel.rb
@@ -0,0 +1,37 @@
+class Radiodan
+class ChangeChannel
+  include Logging
+
+  def initialize(config)
+    @filename = config[:filename]
+  end
+
+  def call(player)
+    @player = player
+
+    @player.register_event :change_channel do
+      change_channel!
+    end
+  end
+
+  def change_channel!
+
+    EM.defer \
+      proc {
+        contents = File.read(@filename)
+        contents = contents.chomp
+        arr = contents.split("\t")
+        new_channel = arr[0]
+        url = arr[1]
+        logger.debug "changing channel to #{new_channel}, #{url}..."
+        playlist = Radiodan::Playlist.new tracks: url
+        @player.playlist = playlist
+      },
+      proc {
+      }
+
+  end
+
+end
+end
+
